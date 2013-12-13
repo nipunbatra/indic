25dccd916a3bd91e15b8b9517250c0fac45578d6
nipunreddevil <nipunreddevil@gmail.com>
2013-12-13 13:21:05 +0530

    Fixed PEP8

--- examples/redd.py
+++ examples/redd.py
@@ -18,66 +18,61 @@ import sys
 
 path = sys.argv[1]
 print 'Loading Mains Data'
-df_mains=load_mains_data(path)
-labels=load_labels(path)
+df_mains = load_mains_data(path)
+labels = load_labels(path)
 print 'Loading appliance Data'
-df_appliances=load_appliances_data(path,labels)
+df_appliances = load_appliances_data(path, labels)
 
-#Downsampling
+# Downsampling
 print 'Downsampling'
-downsampling_window='30Min'
-df_mains_downsampled=downsample(df_mains,downsampling_window)
-df_appliances_downsampled=downsample(df_appliances,downsampling_window)
+downsampling_window = '30Min'
+df_mains_downsampled = downsample(df_mains, downsampling_window)
+df_appliances_downsampled = downsample(df_appliances, downsampling_window)
 
 print 'Aligning time series'
-#Aligning mains and appliances time series
-common_index=find_intersection(df_mains_downsampled, df_appliances_downsampled)
-df_mains_downsampled_aligned=df_mains_downsampled.ix[common_index]
-df_appliances_downsampled_aligned=df_appliances_downsampled.ix[common_index]
+# Aligning mains and appliances time series
+common_index = find_intersection(
+    df_mains_downsampled, df_appliances_downsampled)
+df_mains_downsampled_aligned = df_mains_downsampled.ix[common_index]
+df_appliances_downsampled_aligned = df_appliances_downsampled.ix[common_index]
 
 print 'Removing appliances'
-#Removing appliances whose contribution is insignificant
-df_appliances_downsampled_aligned_remove_insignificant=reject_insignificant_appliance(df_appliances_downsampled_aligned)
+# Removing appliances whose contribution is insignificant
+df_appliances_downsampled_aligned_remove_insignificant = reject_insignificant_appliance(
+    df_appliances_downsampled_aligned)
 
 print 'Loads to mains_mapping'
-#Loads to mains mapping
-loads_to_mains_mapping=assign_load_to_mains(df_mains_downsampled_aligned, df_appliances_downsampled_aligned_remove_insignificant)
+# Loads to mains mapping
+loads_to_mains_mapping = assign_load_to_mains(
+    df_mains_downsampled_aligned, df_appliances_downsampled_aligned_remove_insignificant)
 
 for app in df_appliances_downsampled_aligned_remove_insignificant:
     if app not in loads_to_mains_mapping:
-        print "DELETING",app
+        print "DELETING", app
         del df_appliances_downsampled_aligned_remove_insignificant[app]
 
 print loads_to_mains_mapping
 
-#Dividing the data into test and train
-[df_train_mains,df_train_appliances,df_test_mains,df_test_appliances]=partition_train_test(df_mains_downsampled_aligned, df_appliances_downsampled_aligned_remove_insignificant)
+# Dividing the data into test and train
+[df_train_mains, df_train_appliances, df_test_mains, df_test_appliances] = partition_train_test(
+    df_mains_downsampled_aligned, df_appliances_downsampled_aligned_remove_insignificant)
 
 print df_appliances_downsampled_aligned_remove_insignificant.describe()
 print 'Clustering'
 # Finding the centroids and labels assigned to the data
-[centroids,labels]=return_centroids_labels(df_train_appliances)
+[centroids, labels] = return_centroids_labels(df_train_appliances)
 
-#Finding the calibrated centroids
-calib_centroids=calibrate_centroids(df_train_mains, df_train_appliances, labels, centroids, loads_to_mains_mapping)
+# Finding the calibrated centroids
+calib_centroids = calibrate_centroids(
+    df_train_mains, df_train_appliances, labels, centroids, loads_to_mains_mapping)
 
 print 'Performing CO'
-#Peform CO
-power_states_dict=apply_co(centroids,calib_centroids,loads_to_mains_mapping, df_test_mains)
+# Peform CO
+power_states_dict = apply_co(
+    centroids, calib_centroids, loads_to_mains_mapping, df_test_mains)
 
-#Compute results
-[MNE,RE]=compute_RE_MNE(power_states_dict, df_test_appliances)
+# Compute results
+[MNE, RE] = compute_RE_MNE(power_states_dict, df_test_appliances)
 
-#Draw table
-draw_table(MNE,RE,str(house))
-
-
-
-
-
-
-
-
-
-
-
+# Draw table
+draw_table(MNE, RE, str(house))
